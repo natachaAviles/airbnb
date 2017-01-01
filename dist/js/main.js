f611db7a5c4b05426921a15a1e6d6c26e036398c
JocelynJoe <jocelyn.pena.contreras@gmail.com>
2016-12-31 19:53:13 -0800

    avance segunda pagina range pendiente falta js

--- dist/js/main.js
+++ dist/js/main.js
@@ -28,4 +28,24 @@
           }
         });
       }
-  
+      
+$(function () {
+
+        $("#range").ionRangeSlider({
+            hide_min_max: true,
+            keyboard: true,
+            min: 0,
+            max: 5000,
+            from: 1000,
+            to: 4000,
+            type: 'double',
+            step: 1,
+            prefix: "$",
+            grid: true
+        });
+
+    });  
+
+ $(document).ready(function(){
+    $('.tooltipped').tooltip({delay: 50});
+  });
--- src/js/search.js
+++ src/js/search.js
@@ -1,9 +1,3 @@
-$("#range_03").ionRangeSlider({
-    type: "double",
-    grid: true,
-    min: 0,
-    max: 1000,
-    from: 200,
-    to: 800,
-    prefix: "$"
-});
+ $(document).ready(function(){
+    $('.tooltipped').tooltip({delay: 50});
+  });
