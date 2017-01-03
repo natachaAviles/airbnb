b242d9799a63bca30befd736205e8f07b28acbcd
JocelynJoe <jocelyn.pena.contreras@gmail.com>
2017-01-02 23:31:46 -0800

    mapa

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,41 @@
 $(document).ready(function() {
-  $('select').material_select();
-  $('.datepicker').pickadate({
+	$('select').material_select();
+	$('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
+});
 });
 
- $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
-        
- 
+$(document).ready(function(){
+	$('.slider').slider({full_width: true});
+});
+
+function initMap() {
+	var directionsService = new google.maps.DirectionsService;
+	var directionsDisplay = new google.maps.DirectionsRenderer;
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 7,
+		center: {lat: 41.85, lng: -87.65}
+	});
+	directionsDisplay.setMap(map);
+
+	var onChangeHandler = function() {
+		calculateAndDisplayRoute(directionsService, directionsDisplay);
+	};
+	document.getElementById('start').addEventListener('change', onChangeHandler);
+	document.getElementById('end').addEventListener('change', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+	directionsService.route({
+		origin: document.getElementById('start').value,
+		destination: document.getElementById('end').value,
+		travelMode: 'DRIVING'
+	}, function(response, status) {
+		if (status === 'OK') {
+			directionsDisplay.setDirections(response);
+		} else {
+			window.alert('Directions request failed due to ' + status);
+		}
+	});
+}
--- src/js/search.js
+++ src/js/search.js
@@ -1,3 +1,21 @@
  $(document).ready(function(){
     $('.tooltipped').tooltip({delay: 50});
+  	$("#range").ionRangeSlider({
+            hide_min_max: true,
+            keyboard: true,
+            min: 0,
+            max: 50000,
+            from: 6000,
+            to: 50000,
+            type: 'double',
+            step: 1,
+            prefix: "$",
+            grid: true
+     });
+
+
   });
+
+
+
+
